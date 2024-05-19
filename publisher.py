5a721532ca8442d2160eb06214a1be81d4534a5d
iXenios <schmidt_fabio@web.de>
2024-05-19 19:32:59 +0200

    publisher updated

--- publisher.py
+++ publisher.py
@@ -1,13 +1,15 @@
 import paho.mqtt.client as mqtt
 from pymongo import MongoClient
+import json
+import time
 
 # MongoDB connection
 client = MongoClient('mongodb://localhost:27017/')
-db = client['VM']
+db = client['VM_DB']
 collection = db['Test_Data']
 
 # MQTT broker details
-mqtt_broker = 'localhost'  # or specify the IP address of your VM
+mqtt_broker = 'localhost'
 mqtt_port = 1883
 mqtt_topic = 'fbs_vierplus'
 
@@ -15,9 +17,24 @@ mqtt_topic = 'fbs_vierplus'
 mqtt_client = mqtt.Client()
 mqtt_client.connect(mqtt_broker, mqtt_port)
 
-# Publish data from MongoDB to MQTT topic
-for document in collection.find():
-    data_to_publish = str(document)  # Modify as needed
-    mqtt_client.publish(mqtt_topic, data_to_publish)
+# Function to fetch new data from MongoDB and publish to MQTT
+def fetch_and_publish():
+    last_id = None
+    
+    while True:
+        query = {}
+        if last_id:
+            query = {'_id': {'$gt': last_id}}  # Query for documents with an ID greater than the last processed ID
+        
+        documents = collection.find(query).sort('_id')
+        for document in documents:
+            data_to_publish = json.dumps(document)  # Convert MongoDB document to JSON
+            mqtt_client.publish(mqtt_topic, data_to_publish)
+            last_id = document['_id']  # Update last_id to the ID of the current document
+        
+        time.sleep(10)  # Wait for a specified time before checking for new data again
+
+# Start fetching and publishing data
+fetch_and_publish()
 
 mqtt_client.disconnect()
